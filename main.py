9fcd0bf6a37e9d0b85455256060b7caca995a29b
Kallahan23 <armkalnian@gmail.com>
2017-08-26 09:56:16 +1000

    Commenting

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 '''
-Camera Example
+Python Camera
 ==============
-
-This example demonstrates a simple use of the camera. It shows a window with
-a buttoned labelled 'play' to turn the camera on and off. Note that
-not finding a camera, perhaps because gstreamer is not installed, will
-throw an exception during the kv language processing.
 
 '''
 
